facac78d36bb29dd241451ca3bc8f3cd0f6f48b2
Glitch (mountainous-fearless-shrimp) <none>
2020-05-18 06:38:21 +0000

    🐮🐀 Checkpoint ./routes/index.js:3893646/12

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const express = require("express");
 // create route handler
 const router = express.Router();
 
-// GET home page.
-router.get('/', function(req, res) {
+// GET home page
+router.get('/', (req, res) => {
   res.redirect("/views/index.html");
 });
 
