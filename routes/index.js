d5190fd73aef0419ea523d8dc3a1124948f37b08
Glitch (mountainous-fearless-shrimp) <none>
2020-05-18 10:27:19 +0000

    ✂️🎹 Checkpoint ./routes/index.js:3893646/21

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 // GET home page
 router.get('/', (req, res) => {
-  res.redirect("/views");
+  res.redirect("/views/index.html");
 });
 
 module.exports = router;
